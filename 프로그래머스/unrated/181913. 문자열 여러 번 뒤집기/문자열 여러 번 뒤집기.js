90aa1e36b658f7d357a1b19932be537f4051a701
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-06-13 19:42:21 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 43.19 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,9 +1,11 @@
 function solution(my_string, queries) {
-    my_string = my_string.split("");
-    for(let i=0;i<queries.length;i++) {
-        const [s, e] = queries[i];
-        my_string = [...my_string.slice(0, s), ...my_string.slice(s, e + 1).reverse(), ...my_string.slice(e + 1)];
+    my_string = my_string.split(""); //문자열을 reverse 함수를 쓸수 없기 때문에 배열로 바꿔준다.
+    for(let i=0;i<queries.length;i++) { 
+        const [s, e] = queries[i]; // queries 돌면서 s e 에 배열값을 넣는다 
+        my_string=[...my_string.slice(0,s),
+                  ...my_string.slice(s,e+1).reverse(),
+                  ...my_string.slice(e+1)]
     }
-
-    return my_string.join("");
+    return my_string.join(""); //문자열로 다시 바꿔준다.
 }
+
