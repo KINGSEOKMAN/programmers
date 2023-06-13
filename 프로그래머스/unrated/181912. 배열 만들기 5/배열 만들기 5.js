954556eac5a79727d6b524c5c941482c8815fbc3
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-06-13 20:45:25 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.55 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,11 +1,10 @@
+
 function solution(intStrs, k, s, l) {
      const arr=[];
     for(i=0; i<intStrs.length; i++){
-        arr.push(intStrs[i].substr(s,l));
+        arr.push(parseInt(intStrs[i].substr(s,l)));
     }
-    const intArr = arr.map(strNum => parseInt(strNum))
-    const result = intArr.filter(num=> num>k);
-    
+    const result = arr.filter(num=> num>k)
     return result;
-  
+
 }
