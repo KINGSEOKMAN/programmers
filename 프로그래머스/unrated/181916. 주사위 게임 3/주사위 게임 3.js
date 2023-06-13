72f5fc43c02ab5c12d262cc3da348a19497b8a4f
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-06-13 18:56:15 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,36 +1,21 @@
 function solution(a, b, c, d) {
-    const dices = [a, b, c, d]
-    const numFrequency = dices.reduce((acc, cur) => {
-        acc[cur] = acc[cur] ?? 0
-        acc[cur]++
-        return acc
-    }, {})
-    
-    const valInfo = Object.values(numFrequency)
-    const keyInfo = Object.keys(numFrequency)
-    // return valInfo
-    const countVal = Math.max(...valInfo)
-    switch(countVal) {
-        case 4:
-            return a*1111
-        case 3: {
-            const threeKey  = keyInfo.find((a) => numFrequency[a] === 3)
-            const oneKey = keyInfo.find((a) => numFrequency[a] === 1)
-            console.log(threeKey, oneKey)
-            return (10 * Number(threeKey) + Number(oneKey))**2
-        }
-        case 2: {
-            if(keyInfo.length === 2) {
-                if(a === b) return (a+c) * Math.abs(a-c)
-                return (a+b) * Math.abs(a-b)
-            } else {
-                return  keyInfo.reduce((acc, cur) => numFrequency[cur] === 1 ? acc*cur : acc, 1)
-            }
-        }   
-        default: {
-            return Math.min(a,b,c,d)
-        }
-    }
-    
-    return numFrequency
+    if (a === b && a === c && a === d) return 1111 * a
+
+    if (a === b && a === c) return (10 * a + d) ** 2
+    if (a === b && a === d) return (10 * a + c) ** 2
+    if (a === c && a === d) return (10 * a + b) ** 2
+    if (b === c && b === d) return (10 * b + a) ** 2
+
+    if (a === b && c === d) return (a + c) * Math.abs(a - c)
+    if (a === c && b === d) return (a + b) * Math.abs(a - b)
+    if (a === d && b === c) return (a + b) * Math.abs(a - b)
+
+    if (a === b) return c * d
+    if (a === c) return b * d
+    if (a === d) return b * c
+    if (b === c) return a * d
+    if (b === d) return a * c
+    if (c === d) return a * b
+
+    return Math.min(a, b, c, d)
 }
