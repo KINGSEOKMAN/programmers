1919331b338c64e5f9bc42c3f853c9ecb424d8ba
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-06-13 19:31:58 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 37.76 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,9 +1,9 @@
 function solution(my_string, queries) {
-  for (let i = 0; i < queries.length; i++) {
-    const [start, end] = queries[i];
-    const substring = my_string.substring(start, end + 1);
-    const reversed = substring.split("").reverse().join("");
-    my_string = my_string.substring(0, start) + reversed + my_string.substring(end + 1);
-  }
-  return my_string;
+    my_string = my_string.split("");
+    for(let i=0;i<queries.length;i++) {
+        const [s, e] = queries[i];
+        my_string = [...my_string.slice(0, s), ...my_string.slice(s, e + 1).reverse(), ...my_string.slice(e + 1)];
+    }
+
+    return my_string.join("");
 }
