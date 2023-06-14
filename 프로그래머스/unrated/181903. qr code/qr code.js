8e6f296ee0d493bf93982d9e51da530e957fd705
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-06-14 19:11:26 +0900

    [unrated] Title: qr code, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.js
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -1,11 +1,7 @@
 function solution(q, r, code) {
-    let str=[];
-    let arr=code.split('');
-    
-    for(i=0; i<arr.length; i++){
-        if(i%q==r){
-            str.push(arr[i]);
-        }
-    }
-    return str.join('');
+    var answer = '';
+
+    code.split("").forEach((e,i) => {if(i % q == r) answer += e});
+
+    return answer;
 }
