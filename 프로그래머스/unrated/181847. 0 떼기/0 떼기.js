394678885cd9a68e311901c6964c04bd0b439301
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-06-29 20:04:47 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,7 +1,6 @@
 function solution(n_str) {
     for(let i = 0; i < n_str.length; i++){
-        const num = n_str[i];
-        if(0!==parseInt(num)){
+        if('0'!==n_str[i]){
             return n_str.slice(i)
         }
     }
