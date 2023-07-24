b283eec1619f41c0148f1e66279ac04803ae1453
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-07-24 23:06:58 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,12 +1,6 @@
+
 function solution(emergency) {
-    const sorted = emergency.slice().sort((a, b) => b - a); // 원본 배열을 변경하지 않기 위해 복사한 배열을 정렬
-    const arr = [];
-
-
-    for (let i = 0; i < emergency.length; i++) {
-        const index = sorted.indexOf(emergency[i]);
-        arr.push(index+1);
-    }
-
-    return arr;
+    const rank = [...emergency];
+    rank.sort((a, b) => b - a);
+    return emergency.map(n => rank.indexOf(n) + 1);
 }
