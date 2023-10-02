38cbf930436eec05bd92d65939995f195d849328
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-10-02 23:29:24 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,8 +1,4 @@
 function solution(numbers) {
-  numbers.sort((a, b) => a - b);
-
-  const maxProduct = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-  const minProduct = numbers[0] * numbers[1];
-  
-  return Math.max(maxProduct, minProduct);
+    var answer = 0;
+    return answer;
 }
