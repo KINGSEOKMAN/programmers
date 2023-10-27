efd10af24012fc543e9eed12b85e18101316fc1a
KINGSEOKMAN <135989685+KINGSEOKMAN@users.noreply.github.com>
2023-10-27 18:26:08 +0900

    [level unrated] Title: 평행, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120875. 평행/평행.js
+++ 프로그래머스/unrated/120875. 평행/평행.js
@@ -1,12 +1,6 @@
-function solution(dots) {
-    let answer = 0;
-    const total = [0,1,2,3]
-    const arr = [[0,1],[0,2],[0,3],[1,2],[1,3],[2,3]]
-    for(let i of arr){
-        const rest = total.filter((x)=>!i.includes(x))
-        const first = (dots[i[0]][1] - dots[i[1]][1]) / (dots[i[0]][0]-dots[i[1]][0])
-        const second = (dots[rest[0]][1] - dots[rest[1]][1]) / (dots[rest[0]][0]-dots[rest[1]][0])
-        if(first === second) answer =1
-    }
-    return answer;
+const solution = ([[x1, y1], [x2, y2], [x3, y3], [x4, y4]]) => {
+    if((y1 - y2) / (x1 - x2) === (y3 - y4) / (x3 - x4)) return 1;
+    if((y1 - y3) / (x1 - x3) === (y2 - y4) / (x2 - x4)) return 1;
+    if((y1 - y4) / (x1 - x4) === (y2 - y3) / (x2 - x3)) return 1;
+    return 0;
 }
